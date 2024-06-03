0cbd904b98eaaf2b61bae47124a862909ffbf637
Nabi <nabijonazamov@gmail.com>
2024-06-03 10:40:08 +0500

    impl-ed color js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ const fs = require("fs");
 const axios = require("axios");
 const directoryPath = require("./dir");
 const url = require("./arg")("url");
+const colors = require("colors");
 
-console.log("request to:", url);
+console.log(colors.green("request to:"), url);
+const files = [];
 
 async function copyAll(path, folder = "") {
   try {
@@ -12,28 +14,42 @@ async function copyAll(path, folder = "") {
       const itemPath = path + "/" + item;
       const isDir = fs.lstatSync(itemPath).isDirectory();
       if (isDir) {
-        copyAll(itemPath, item);
+        await copyAll(itemPath, item);
       } else {
-        console.log("file to copy", itemPath, folder);
-        var newFile = fs.readFileSync(itemPath);
-        const blob = new Blob([newFile]); // Adjust content type if needed
-        const form_data = new FormData();
-        form_data.append("file", blob, item);
-        const request_config = {
-          method: "post",
-          url,
-          headers: {
-            "Content-Type": "multipart/form-data",
-            folder,
-          },
-          data: form_data,
-        };
-        await axios(request_config);
+        files.push([itemPath, folder, item]);
       }
     }
   } catch (err) {
-    console.error("Error", err);
+    console.error(colors.red("Error sync dir"), err);
+  }
+}
+
+async function sendFiles() {
+  for (let fileItem of files) {
+    const [itemPath, folder, fileName] = fileItem;
+    var newFile = fs.readFileSync(itemPath);
+    const blob = new Blob([newFile]); // Adjust content type if needed
+    const form_data = new FormData();
+    form_data.append("file", blob, fileName);
+    const fileInfo = folder + "/" + fileName;
+    console.log(colors.yellow("file to send:"), fileInfo);
+    const request_config = {
+      method: "post",
+      url,
+      headers: {
+        "Content-Type": "multipart/form-data",
+        folder,
+      },
+      data: form_data,
+    };
+    try {
+      await axios(request_config);
+    } catch (e) {
+      console.error(colors.red("Can't send file"), fileInfo, e.message);
+    }
   }
 }
 
 copyAll(directoryPath);
+console.log(colors.green("total files count to copy: "), files.length);
+sendFiles();
